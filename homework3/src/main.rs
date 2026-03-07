b53dfc10e85b1eacd3c9593fd1a9b880378d8fdc
ChickenJoeAss <chinchi.17m@gmail.com>
2026-03-07 01:33:26 +0000

    Homework3 Book Catalog

---
+++ homework3/src/main.rs
@@ -0,0 +1,67 @@
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead};
+
+struct Book {
+    title: String,
+    author: String,
+    year: u16,
+}
+
+fn save_books(books: &Vec<Book>, filename: &str) {
+    // create the file
+    let mut file = File::create(filename).unwrap();
+
+    // write each book to the file
+    for book in books {
+        writeln!(file, "{},{},{}", book.title, book.author, book.year).unwrap();
+    }
+}
+
+fn load_books(filename: &str) -> Vec<Book> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut books: Vec<Book> = Vec::new();
+
+    // read each line
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        let parts: Vec<&str> = line.split(',').collect();
+
+        let book = Book {
+            title: parts[0].to_string(),
+            author: parts[1].to_string(),
+            year: parts[2].parse::<u16>().unwrap(),
+        };
+
+        books.push(book);
+    }
+
+    books
+}
+
+fn main() {
+    let books = vec![
+        Book {
+            title: "1984".to_string(),
+            author: "George Orwell".to_string(),
+            year: 1949,
+        },
+        Book {
+            title: "To Kill a Mockingbird".to_string(),
+            author: "Harper Lee".to_string(),
+            year: 1960,
+        },
+    ];
+
+    save_books(&books, "books.txt");
+    println!("Books saved to file.");
+
+    let loaded_books = load_books("books.txt");
+
+    println!("Loaded books:");
+    for book in loaded_books {
+        println!("{} by {}, published in {}", book.title, book.author, book.year);
+    }
+}
